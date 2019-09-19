ff0b9bbc6e6654e9a1b9ff16dbf463d435d0193e
bclip <hjcho@frontech.co.kr>
2019-09-19 23:09:01 +0900

    대기방 리스트 어뎁터

---
+++ app/src/main/java/com/wlswnwns/chosung_android/item/User.kt
@@ -0,0 +1,8 @@
+package com.wlswnwns.chosung_android.item
+
+class User{
+
+    var strUserName : String = ""
+
+
+}
--- app/src/main/java/com/wlswnwns/chosung_android/waitRoom/WaitRoomFragment.kt
+++ app/src/main/java/com/wlswnwns/chosung_android/waitRoom/WaitRoomFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.navArgs
 import com.wlswnwns.chosung_android.R
-import com.wlswnwns.chosung_android.main.MainPresenter
-import com.wlswnwns.chosung_android.nickname.NickNameFragmentDirections
+import com.wlswnwns.chosung_android.adapter.WaitRoomUserListAdapter
+import com.wlswnwns.chosung_android.item.User
 import kotlinx.android.synthetic.main.layout_wait_room.*
 
 class WaitRoomFragment : Fragment(),WaitRoomContract.View{
@@ -41,6 +43,8 @@ class WaitRoomFragment : Fragment(),WaitRoomContract.View{
     override fun viewInit() {
         HunMinBtn.setOnClickListener { moveHunMinFragment() }
         KungKungDdaBtn.setOnClickListener { moveKungKungDdaFragment() }
+        GameStartBtn.setOnClickListener { presenter?.onClickGameStartBtn() }
+        BackBtn.setOnClickListener { presenter?.onClickExitRoom() }
     }
 
     override fun moveHunMinFragment() {
@@ -57,14 +61,36 @@ class WaitRoomFragment : Fragment(),WaitRoomContract.View{
     }
 
     override fun showEixtRoomDialog() {
-
+        AlertDialog.Builder(requireContext()).apply {
+            setTitle(R.string.exit_wait_room_dialog_title)
+            setMessage(R.string.exit_wait_room_dialog_content)
+            setPositiveButton(R.string.dialog_confirm_btn) { _, _ -> presenter?.checkRoomOwner() }
+            setNegativeButton(R.string.dialog_cancel_btn){ dialoginterface,_ -> dialoginterface.cancel()  }
+            create()
+            show()
+        }
     }
 
     override fun exitRoom() {
-
+        ((requireActivity().supportFragmentManager
+            .findFragmentById(R.id.fragment_container) as NavHostFragment)
+            .navController).popBackStack()
     }
 
     override fun showFailExitRoom() {
+
+    }
+
+    override fun showUserList(Users: ArrayList<User>) {
+
+        WaitRoomUserListAdapter(
+            requireContext(),
+            presenter!!,
+            Users
+        ).let {
+            entryListView.adapter = it
+        }
+
 
     }
 
