5bb952497a3f6865e3292f73776fdbc6a7500841
peopet-steve <steve.kim@peopet.co.kr>
2019-11-03 15:06:21 +0900

    User 모델에 파라미터 추가

--- app/src/main/java/com/wlswnwns/chosung_android/item/User.kt
+++ app/src/main/java/com/wlswnwns/chosung_android/item/User.kt
@@ -3,5 +3,8 @@ package com.wlswnwns.chosung_android.item
 class User{
 
     var strUserName : String = ""
+    var bIsActive : Boolean = false
+    var bIsMaster : Boolean= false
+    var iOrder : Int = 0
 }
 
